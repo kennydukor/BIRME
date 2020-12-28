6fe5fc5bafd634c2c8fd2bfd7df34ed919a10593
Kenechi <kennydukor@gmail.com>
2020-12-28 01:36:42 +0100

    adding weightFunc

---
+++ DCGAN/weightFunc.py
@@ -0,0 +1,12 @@
+class WeightFunc:
+    def __init__(self, model):
+        self.model = model
+
+    # custom weights initialization called on netG and netD
+    def weights_init_0_02(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
