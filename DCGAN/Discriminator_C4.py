447cefe8e447ae909e8cb8d82e87c374e5f15662
Kenechi <kennydukor@gmail.com>
2020-12-30 04:38:12 +0100

    adding discriminator

---
+++ DCGAN/Discriminator_C4.py
@@ -0,0 +1,35 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu, nc, ndf):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.nc = nc
+        self.ndf = ndf
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(self.nc, self.ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(self.ndf, self.ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(self.ndf * 2, self.ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(self.ndf * 4, self.ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(self.ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
--- DCGAN/weightFunc.py
+++ DCGAN/weightFunc.py
@@ -1,15 +1,15 @@
 import torch.nn as nn
 import os
 
-# custom weights initialization called on netG and netD
-def weights_init_0_02(m):
-
-'''From the DCGAN paper, the authors specify that all model weights shall be randomly initialized 
+""" From the DCGAN paper, the authors specify that all model weights shall be randomly initialized 
 from a Normal distribution with mean=0, stdev=0.02. 
 The weights_init_0_02 function takes an initialized model as input and reinitializes all convolutional, 
 convolutional-transpose, and batch normalization layers to meet this criteria. 
-This function is applied to the models immediately after initialization.'''
+This function is applied to the models immediately after initialization.
+"""
 
+# custom weights initialization called on netG and netD
+def weights_init_0_02(m): 
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
